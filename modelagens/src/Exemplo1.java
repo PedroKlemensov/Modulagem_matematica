c590769d7977eb8e4c51ebe4cfc9d666b074cb44
Pedroklemensov <80336621+PedroKlemensov@users.noreply.github.com>
2023-02-09 11:43:08 -0300

    a

---
+++ modelagens/src/Exemplo1.java
@@ -0,0 +1,53 @@
+import com.google.ortools.Loader;
+import com.google.ortools.linearsolver.MPConstraint;
+import com.google.ortools.linearsolver.MPObjective;
+import com.google.ortools.linearsolver.MPSolver;
+import com.google.ortools.linearsolver.MPVariable;
+
+public class Exemplo1 {
+
+    public static void main(String[] args) {
+        Loader.loadNativeLibraries();
+
+        MPSolver solver = MPSolver.createSolver("SCIP");
+        double infinity = java.lang.Double.POSITIVE_INFINITY;
+
+        // x, y >= 0 e inteiros
+        MPVariable x = solver.makeIntVar(0.0, infinity, "x");
+        MPVariable y = solver.makeIntVar(0.0, infinity, "y");
+        System.out.println("Número de variáveis = " + solver.numVariables());
+
+        // x + 7 * y <= 17.5.
+        MPConstraint c0 = solver.makeConstraint(-infinity, 17.5, "c0");
+        c0.setCoefficient(x, 1);
+        c0.setCoefficient(y, 7);
+
+        // x <= 3.5.
+        MPConstraint c1 = solver.makeConstraint(-infinity, 3.5, "c1");
+        c1.setCoefficient(x, 1);
+        c1.setCoefficient(y, 0);
+
+        System.out.println("Número de restrições = " + solver.numConstraints());
+
+        // Maximize x + 10 * y.
+        MPObjective objective = solver.objective();
+        objective.setCoefficient(x, 1);
+        objective.setCoefficient(y, 10);
+        objective.setMaximization();
+
+        MPSolver.ResultStatus resultStatus = solver.solve();
+
+        if (resultStatus == MPSolver.ResultStatus.OPTIMAL) {
+            System.out.println("Solução:");
+            System.out.println("Custo da função objetivo = " + objective.value());
+            System.out.println("x = " + x.solutionValue());
+            System.out.println("y = " + y.solutionValue());
+            System.out.println("Tempo de resolução = " + solver.wallTime() + " milissegundos");
+            System.out.println(solver.exportModelAsLpFormat());
+        } else {
+            System.out.println("Solução ótima não encontrada!");
+        }
+
+    }
+
+} 
